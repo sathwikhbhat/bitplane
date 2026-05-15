8ac2b65e496f27fe82cc66fd929335a16b47c434
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-05-15 14:44:32 +0530

    feat: add binary header and metadata packing into payload

--- src/main/java/com/sathwikhbhat/encoder/FileToImageEncoder.java
+++ src/main/java/com/sathwikhbhat/encoder/FileToImageEncoder.java
@@ -2,6 +2,7 @@ package com.sathwikhbhat.encoder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sathwikhbhat.protocol.Metadata;
+import com.sathwikhbhat.util.ByteUtil;
 import com.sathwikhbhat.util.ImageDimensionUtil;
 
 import javax.imageio.ImageIO;
@@ -19,6 +20,8 @@ import static com.sathwikhbhat.constants.ImageConstants.ENCODED_IMAGE_PATH;
 public class FileToImageEncoder {
     public void encode(Path inputFile) throws IOException {
         byte[] fileBytes = Files.readAllBytes(inputFile);
+        byte[] fileLengthBytes = ByteUtil.longToBytes(fileBytes.length);
+
         System.out.println("File read successfully. Size: " + fileBytes.length + " bytes");
 
         if (fileBytes.length == 0) {
@@ -35,9 +38,30 @@ public class FileToImageEncoder {
         String metadataJson = mapper.writeValueAsString(metadata);
 
         byte[] metadataBytes = metadataJson.getBytes(StandardCharsets.UTF_8);
+        byte[] metadataLengthBytes = ByteUtil.longToBytes(metadataBytes.length);
 
-        Dimension dimension = ImageDimensionUtil.calculateDimensions(fileBytes.length);
+        int totalPayloadSize =
+                metadataLengthBytes.length
+                        + fileLengthBytes.length
+                        + metadataBytes.length
+                        + fileBytes.length;
+
+        Dimension dimension = ImageDimensionUtil.calculateDimensions(totalPayloadSize);
         System.out.println("Calculated image dimensions: " + dimension.width + "x" + dimension.height);
+
+        byte[] payloadBytes = new byte[totalPayloadSize];
+        int offset = 0;
+
+        System.arraycopy(metadataLengthBytes, 0, payloadBytes, offset, metadataLengthBytes.length);
+        offset += metadataLengthBytes.length;
+
+        System.arraycopy(fileLengthBytes, 0, payloadBytes, offset, fileLengthBytes.length);
+        offset += fileLengthBytes.length;
+
+        System.arraycopy(metadataBytes, 0, payloadBytes, offset, metadataBytes.length);
+        offset += metadataBytes.length;
+
+        System.arraycopy(fileBytes, 0, payloadBytes, offset, fileBytes.length);
 
         BufferedImage image = new BufferedImage(
                 dimension.width,
@@ -47,9 +71,9 @@ public class FileToImageEncoder {
         WritableRaster raster = image.getRaster();
 
         int index = 0;
-        for (int y = 0; y < dimension.height && index < fileBytes.length; y++) {
-            for (int x = 0; x < dimension.width && index < fileBytes.length; x++, index++) {
-                int value = fileBytes[index] & 0xFF;
+        for (int y = 0; y < dimension.height && index < payloadBytes.length; y++) {
+            for (int x = 0; x < dimension.width && index < payloadBytes.length; x++, index++) {
+                int value = payloadBytes[index] & 0xFF;
                 raster.setSample(x, y, 0, value);
             }
         }
