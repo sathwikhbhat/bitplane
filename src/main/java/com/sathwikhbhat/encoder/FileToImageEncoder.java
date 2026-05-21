b308e5c5d6e3bc2ac5a7751b7801e0d429674e7e
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-05-21 16:29:42 +0530

    refactor: split file encoding pipeline into modular methods

--- src/main/java/com/sathwikhbhat/encoder/FileToImageEncoder.java
+++ src/main/java/com/sathwikhbhat/encoder/FileToImageEncoder.java
@@ -19,8 +19,19 @@ import static com.sathwikhbhat.constants.ImageConstants.ENCODED_IMAGE_PATH;
 
 public class FileToImageEncoder {
     public void encode(Path inputFile) throws IOException {
+        byte[] fileBytes = readFileBytes(inputFile);
+
+        Metadata metadata = createMetadata(inputFile, fileBytes);
+
+        byte[] payloadBytes = createPayload(metadata, fileBytes);
+
+        BufferedImage image = createImage(payloadBytes);
+
+        saveImage(image);
+    }
+
+    private byte[] readFileBytes(Path inputFile) throws IOException {
         byte[] fileBytes = Files.readAllBytes(inputFile);
-        byte[] fileLengthBytes = ByteUtil.longToBytes(fileBytes.length);
 
         System.out.println("File read successfully. Size: " + fileBytes.length + " bytes");
 
@@ -28,9 +39,19 @@ public class FileToImageEncoder {
             throw new IllegalArgumentException("File is empty");
         }
 
+        return fileBytes;
+    }
+
+    private Metadata createMetadata(Path inputFile, byte[] fileBytes) {
         Metadata metadata = new Metadata(inputFile.getFileName().toString(), fileBytes.length);
 
         System.out.println("Metadata created: " + metadata);
+
+        return metadata;
+    }
+
+    private byte[] createPayload(Metadata metadata, byte[] fileBytes) throws IOException {
+        byte[] fileLengthBytes = ByteUtil.longToBytes(fileBytes.length);
 
         ObjectMapper mapper = new ObjectMapper();
         String metadataJson = mapper.writeValueAsString(metadata);
@@ -43,9 +64,6 @@ public class FileToImageEncoder {
                         + fileLengthBytes.length
                         + metadataBytes.length
                         + fileBytes.length;
-
-        Dimension dimension = ImageDimensionUtil.calculateDimensions(totalPayloadSize);
-        System.out.println("Calculated image dimensions: " + dimension.width + "x" + dimension.height);
 
         byte[] payloadBytes = new byte[totalPayloadSize];
         int offset = 0;
@@ -61,22 +79,41 @@ public class FileToImageEncoder {
 
         System.arraycopy(fileBytes, 0, payloadBytes, offset, fileBytes.length);
 
+        return payloadBytes;
+    }
+
+    private BufferedImage createImage(byte[] payloadBytes) {
+        Dimension dimension = ImageDimensionUtil.calculateDimensions(payloadBytes.length);
+
+        System.out.println("Calculated image dimensions: " + dimension.width + "x" + dimension.height);
+
         BufferedImage image = new BufferedImage(
                 dimension.width,
                 dimension.height,
                 BufferedImage.TYPE_BYTE_GRAY
         );
-        WritableRaster raster = image.getRaster();
 
+        WritableRaster raster = image.getRaster();
+        writePayloadToRaster(payloadBytes, raster, dimension);
+
+        return image;
+    }
+
+    private void writePayloadToRaster(byte[] payloadBytes, WritableRaster raster, Dimension dimension) {
         int index = 0;
+
         for (int y = 0; y < dimension.height && index < payloadBytes.length; y++) {
             for (int x = 0; x < dimension.width && index < payloadBytes.length; x++, index++) {
                 int value = payloadBytes[index] & 0xFF;
+
                 raster.setSample(x, y, 0, value);
             }
         }
+    }
 
+    private void saveImage(BufferedImage image) throws IOException {
         Path outputFile = Path.of(ENCODED_IMAGE_PATH);
+
         ImageIO.write(image, "png", outputFile.toFile());
 
         System.out.println("Image saved successfully to " + outputFile.toAbsolutePath());
