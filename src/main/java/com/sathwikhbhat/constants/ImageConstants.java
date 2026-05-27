74fe170cf3c1d8ed01967fdd0def5b867367b16c
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-05-27 22:11:13 +0530

    feat: implement PayloadFrameBuilder for payload segmentation

--- src/main/java/com/sathwikhbhat/constants/ImageConstants.java
+++ src/main/java/com/sathwikhbhat/constants/ImageConstants.java
@@ -8,8 +8,7 @@ public final class ImageConstants {
     public static final int WIDTH = 1920;
     public static final int HEIGHT = 1080;
 
-    public static final int HEADER_SIZE = 16;
-    public static final int LONG_BYTES = 8;
+    public static final int FRAME_BYTE_CAPACITY = WIDTH * HEIGHT;
 
     public static final String INPUT_FILE_NAME = "data/input/sample.txt";
     public static final String ENCODED_IMAGE_PATH = "data/encoded/encoded.png";
