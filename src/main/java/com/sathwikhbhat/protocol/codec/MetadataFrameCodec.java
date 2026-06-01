507183a617e13195c83aec19eaed3032213fec69
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-06-01 16:58:27 +0530

    feat: optimize serialization of MetadataFrame using ByteBuffer

--- src/main/java/com/sathwikhbhat/protocol/codec/MetadataFrameCodec.java
+++ src/main/java/com/sathwikhbhat/protocol/codec/MetadataFrameCodec.java
@@ -3,7 +3,6 @@ package com.sathwikhbhat.protocol.codec;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sathwikhbhat.protocol.frame.MetadataFrame;
-import com.sathwikhbhat.util.ByteUtil;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -14,18 +13,12 @@ public class MetadataFrameCodec {
 
     public byte[] serialize(MetadataFrame metadataFrame) throws JsonProcessingException {
         byte[] metadataBytes = mapper.writeValueAsBytes(metadataFrame);
-        byte[] metadataLengthBytes = ByteUtil.intToBytes(metadataBytes.length);
 
-        int totalMetadataFrameSize = metadataLengthBytes.length + metadataBytes.length;
-
-        byte[] serializedMetadataFrame = new byte[totalMetadataFrameSize];
-
-        int offset = 0;
-        System.arraycopy(metadataLengthBytes, 0, serializedMetadataFrame, offset, metadataLengthBytes.length);
-        offset += metadataLengthBytes.length;
-        System.arraycopy(metadataBytes, 0, serializedMetadataFrame, offset, metadataBytes.length);
-
-        return serializedMetadataFrame;
+        return ByteBuffer
+                .allocate(Integer.BYTES + metadataBytes.length)
+                .putInt(metadataBytes.length)
+                .put(metadataBytes)
+                .array();
     }
 
     public MetadataFrame deserialize(byte[] serializedMetadataFrame) throws IOException {
