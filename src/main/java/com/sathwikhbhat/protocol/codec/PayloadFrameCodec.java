c8b7bc93038e3e8d61328301bea0e322a173217a
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-06-01 16:59:14 +0530

    feat: simplify serialization of PayloadFrame using ByteBuffer

--- src/main/java/com/sathwikhbhat/protocol/codec/PayloadFrameCodec.java
+++ src/main/java/com/sathwikhbhat/protocol/codec/PayloadFrameCodec.java
@@ -1,27 +1,19 @@
 package com.sathwikhbhat.protocol.codec;
 
 import com.sathwikhbhat.protocol.frame.PayloadFrame;
-import com.sathwikhbhat.util.ByteUtil;
 
 import java.nio.ByteBuffer;
 
 public class PayloadFrameCodec {
 
     public byte[] serialize(PayloadFrame payloadFrame) {
-        byte[] frameIndexBytes = ByteUtil.intToBytes(payloadFrame.frameIndex());
         byte[] payload = payloadFrame.payload();
 
-        int totalPayloadFrameSize = frameIndexBytes.length + payload.length;
-
-        byte[] serializedPayloadFrame = new byte[totalPayloadFrameSize];
-
-        int offset = 0;
-
-        System.arraycopy(frameIndexBytes, 0, serializedPayloadFrame, offset, frameIndexBytes.length);
-        offset += frameIndexBytes.length;
-        System.arraycopy(payload, 0, serializedPayloadFrame, offset, payload.length);
-
-        return serializedPayloadFrame;
+        return ByteBuffer
+                .allocate(Integer.BYTES + payload.length)
+                .putInt(payloadFrame.frameIndex())
+                .put(payload)
+                .array();
     }
 
     public PayloadFrame deserialize(byte[] serializedPayloadFrame) {
